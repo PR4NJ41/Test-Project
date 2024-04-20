fcfd5945c2a8f6ff72d71032e954eabaa84ee25f
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2024-04-21 01:12:21 +0530

    Added Navbar

---
+++ src/app/navbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+	return (
+		<ul className="navbar">
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/register">Register</Link>
+            </li>
+            <li>
+                <Link href="/login">Login</Link>
+            </li>
+		</ul>
+	);
+};
+
+export default Navbar;
